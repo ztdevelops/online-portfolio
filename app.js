5ce3647ec20c132c97f7e48d51ddd54bc8f83f39
ztdevelops <yuezhengting@gmail.com>
2022-03-01 17:29:31 +0800

    added active class.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const navigation = document.querySelector('.navigation');
 const navigationToggle = document.querySelector('.navigation-toggle');
+const sections = document.querySelectorAll('section');
+const navigationItems = document.querySelectorAll('header nav ul li')
 
 navigationToggle.addEventListener('click', () => {
     const expanded = navigation.getAttribute('mobile-expanded');
@@ -8,3 +10,21 @@ navigationToggle.addEventListener('click', () => {
     navigation.setAttribute('mobile-expanded', !isExpanded);
     navigationToggle.setAttribute('aria-expanded', !isExpanded);
 })
+
+window.onscroll = () => {
+    var current = "";
+
+    sections.forEach((s) => {
+        const sTop = s.offsetTop;
+        if (scrollY >= sTop - (s.clientHeight / 2)) {
+            current = s.getAttribute('id');
+        }
+    })
+
+    navigationItems.forEach((i) => {
+        i.classList.remove("active");
+        if (i.getAttribute('value') === current) {
+            i.classList.add("active");
+        }
+    })
+}
